2716aba44f7ef75eae387eac03fe5c6fc557d9e6
rosina <rosemat563@gmail.com>
2024-02-01 11:17:56 +0000

    made project pages

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 
-const projectsDisplay = document.querySelector('.projects')
+const portfolioDisplay = document.querySelector('.projects')
 
 fetch('data.json').then(function (response) {
     return response.json() 
 }).then(function (data) {
     data.projects.forEach(function (project) {
-        projectsDisplay.innerHTML += `<div class = "individualpro">
+        portfolioDisplay.innerHTML += `<div class = "individualpro">
         <h2><a href="${project.link}">${project.title}</a></h2>
         <a class="project-img" href="${project.link}"><img class = "image" src="${project.image}"></img></a>
         </div>`
     })
 })
 
-
+// HAVE TO MKAE IF STATEMNT TO PROJECTS ADDED TO JASON ARE NOT ADDED HORIZONTALLY
 
 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("dropdown-content");
   }
+
+
 
 
 //`<h2><a href="${project.link}">${project.title}</a></h2>
@@ -26,18 +28,4 @@ function myFunction() {
 //let photo = document.createElement('img');
 //src = `images/${project.image}`;
 
-//photo.alt = `${project.image}`;
-
-//make a dropdown for contact button for 'linked in, email, phone' and javascript make the phone open the phone app? and email open the email app?
-
-//or 
-
-//make a contact form orrrrr make a separtate contacts page 
-
-//make the separate pages for the projects to link to and put description and fake github link on those 
-
-//javascript for adding the JSON
-//java for the buttons?
-//maybe some cute cool effetcs with javascriot get IDeas 
-//add media queries to make page responsive to mobiles - CSS
-//research what else 
+//photo.alt = `${project.image}`
